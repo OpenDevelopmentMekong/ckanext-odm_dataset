a9550f8edb272c02fb243196be102cc711367aa6
Open-Steps.org <contact@open-steps.org>
2014-09-03 23:24:57 +0200

    First commit
    Homepage replacement is already effective

---
+++ ckanext/odm_theme/plugin.py
@@ -0,0 +1,22 @@
+'''plugin.py
+
+'''
+
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class OdmThemePlugin(plugins.SingletonPlugin):
+    '''ODM theme plugin.
+
+    '''
+    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        # 'templates' is the path to the templates dir, relative to this
+        # plugin.py file.
+        toolkit.add_template_directory(config, 'templates')
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='ckanext-odm_theme',
+    version=version,
+    description="ODM CKAN's theme extension",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Alex Corbi',
+    author_email='mail@lifeformapps.com',
+    url='http://www.lifeformapps.com',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.odm_theme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        odm_theme=ckanext.odm_theme.plugin:OdmThemePlugin
+    ''',
+)
