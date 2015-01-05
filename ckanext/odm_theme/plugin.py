19d0295af5c42ec32996a7e3644a6732468d402d
Open-Steps.org <contact@open-steps.org>
2015-01-05 18:41:17 +0100

    Moved logic from odm_utils

--- ckanext/odm_theme/plugin.py
+++ ckanext/odm_theme/plugin.py
@@ -4,10 +4,11 @@
 import logging
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.navl.validators import (ignore_missing,keep_extras,not_empty,empty,ignore,if_empty_same_as,not_missing,ignore_empty)
 
 log = logging.getLogger(__name__)
 
-def most_popular_groups():    
+def most_popular_groups():
     '''Return a sorted list of the groups with the most datasets.'''
 
     # Get a list of all the site's groups from CKAN, sorted by number of
@@ -21,20 +22,20 @@ def most_popular_groups():
     return groups
 
 def is_user_admin_of_organisation(organization_name):
-    
-    '''Returns wether the current user has the Admin role in the specified organisation'''   
 
-    user_id = toolkit.c.userobj.id        
+    '''Returns wether the current user has the Admin role in the specified organisation'''
 
-    log.debug('is_user_admin_of_organisation: %s %s', user_id, organization_name)         
-    
+    user_id = toolkit.c.userobj.id
+
+    log.debug('is_user_admin_of_organisation: %s %s', user_id, organization_name)
+
     if organization_name is 'None':
 
         return False
 
     try:
 
-        # Retrieve admin members from the specified organisation            
+        # Retrieve admin members from the specified organisation
         members = toolkit.get_action('member_list')(
         data_dict={'id': organization_name, 'object_type': 'user', 'capacity': 'admin'})
 
@@ -47,54 +48,178 @@ def is_user_admin_of_organisation(organization_name):
     member_ids = [member_tuple[0] for member_tuple in members]
 
     try:
-                    
+
         # Finally, we can test whether the user is a member of the curators group.
         if user_id in member_ids:
 
-            return True                    
+            return True
 
     except toolkit.Invalid:
 
         return False
 
     return False
-        
 
-class OdmThemePlugin(plugins.SingletonPlugin):
+class OdmThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     '''ODM theme plugin.
 
     '''
 
-    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IDatasetForm)
     plugins.implements(plugins.IConfigurer)
-
-    # Declare that this plugin will implement ITemplateHelpers.
     plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config):
 
-        # Add this plugin's templates dir to CKAN's extra_template_paths, so
-        # that CKAN will use this plugin's custom templates.
-        # 'templates' is the path to the templates dir, relative to this
-        # plugin.py file.
         toolkit.add_template_directory(config, 'templates')
-
-        # Register this plugin's fanstatic directory with CKAN.
-        # Here, 'fanstatic' is the path to the fanstatic directory
-        # (relative to this plugin.py file), and 'example_theme' is the name
-        # that we'll use to refer to this fanstatic directory from CKAN
-        # templates.
         toolkit.add_resource('fanstatic', 'odm_theme')
 
     def get_helpers(self):
-        '''Register the plugin's functions above as a template
-        helper function.
+        '''Register the plugin's functions above as a template helper function.
 
         '''
-        # Template helper function names should begin with the name of the
-        # extension they belong to, to avoid clashing with functions from
-        # other extensions.
         return {
             'odm_theme_most_popular_groups': most_popular_groups,
             'odm_theme_is_user_admin_of_organisation': is_user_admin_of_organisation
         }
+
+    def _modify_package_schema_write(self, schema):
+        schema.update({
+            'odm_language': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_date_created': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_date_uploaded': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_date_modified': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_temporal_range': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_spatial_range': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_accuracy': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_logical_consistency': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_completeness': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_process': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_access_and_use_constraints': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_metadata_reference_information': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'odm_attributes': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_to_extras')]
+        })
+        schema.update({
+            'taxonomy': [toolkit.get_validator('ignore_missing'),
+              toolkit.get_converter('convert_to_tags')('taxonomy')]
+        })
+
+        return schema
+
+    def _modify_package_schema_read(self, schema):
+        schema.update({
+            'odm_language': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_date_created': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_date_uploaded': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_date_modified': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_temporal_range': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_spatial_range': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_accuracy': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_logical_consistency': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_completeness': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_process': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_access_and_use_constraints': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_metadata_reference_information': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'odm_attributes': [toolkit.get_validator('ignore_missing'),
+                            toolkit.get_converter('convert_from_extras')]
+        })
+        schema.update({
+            'taxonomy': [toolkit.get_validator('ignore_missing'),
+              toolkit.get_converter('convert_from_tags')('taxonomy')]
+        })
+
+        return schema
+
+    def create_package_schema(self):
+        schema = super(OdmThemePlugin, self).create_package_schema()
+        schema = self._modify_package_schema_write(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(OdmThemePlugin, self).update_package_schema()
+        schema = self._modify_package_schema_write(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(OdmThemePlugin, self).show_package_schema()
+        schema = self._modify_package_schema_read(schema)
+        return schema
+
+    def is_fallback(self):
+        return True
+
+    def package_types(self):
+        return []
