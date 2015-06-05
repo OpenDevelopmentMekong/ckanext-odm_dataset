b2761c84ef180eda3b94a58d590899f8acbd6aa6
Alex Corbi <a.corbi@gmail.com>
2015-06-05 13:06:46 +0200

    Fixes #184

--- ckanext/odm_theme/utils/ckanapi_utils.py
+++ ckanext/odm_theme/utils/ckanapi_utils.py
@@ -226,85 +226,91 @@ class RealCkanApi (ICkanApi):
 
   def search_packages(self,params):
 
-    return self.api.call_action('package_search',params)
+    return self.api.call_action('package_search',params,requests_kwargs={'verify':False})
 
   def get_package_list(self,params):
 
-    return self.api.call_action('package_list',params)
+    return self.api.call_action('package_list',params,requests_kwargs={'verify':False})
 
   def get_organization_id_from_name(self,organization):
 
-    return self.api.action.organization_show(id=organization,include_datasets=False)
+    params = {'id':organization,'include_datasets':False}
+    return self.api.call_action('organization_show',params,requests_kwargs={'verify':False})
 
   def add_organization(self,organization,title,description):
 
-    return self.api.action.organization_create(name=organization,title=title,description=description)
+    params = {'name':organization,'title':title,'description':description}
+    return self.api.call_action('organization_create',params,requests_kwargs={'verify':False})
 
   def add_group(self,group,title,description):
 
-    return self.api.action.group_create(name=group,title=title,description=description)
+    params = {'name':group,'title':title,'description':description}
+    return self.api.call_action('group_create',params,requests_kwargs={'verify':False})
 
   def add_user(self,name,email,password,about):
 
-    return self.api.action.user_create(name=name,email=email,password=password,about=about)
+    params = {'name':name,'email':email,'password':password,'about':about}
+    return self.api.call_action('user_create',params,requests_kwargs={'verify':False})
 
   def add_admin_to_organization(self,organization,username,role):
 
-    return self.api.action.organization_member_create(id=organization,username=username,role=role)
+    params = {'id':organization,'username':username,'role':role}
+    return self.api.call_action('organization_member_create',params,requests_kwargs={'verify':False})
 
   def get_package_contents(self,name):
 
-    return self.api.action.package_show(id=name)
+    params = {'id':name}
+    return self.api.call_action('package_show',params,requests_kwargs={'verify':False})
 
   def update_package(self,params):
 
-    return self.api.call_action('package_update',params)
+    return self.api.call_action('package_update',params,requests_kwargs={'verify':False})
 
   def patch_package(self,params):
 
-    return self.api.patch.call_action('package_patch',params)
+    return self.api.patch.call_action('package_patch',params,requests_kwargs={'verify':False})
 
   def create_package(self,params):
 
-    return self.api.call_action('package_create',params)
+    return self.api.call_action('package_create',params,requests_kwargs={'verify':False})
 
   def create_resource(self,params):
 
-    return self.api.call_action('resource_create',params)
+    return self.api.call_action('resource_create',params,requests_kwargs={'verify':False})
 
   def create_resource_with_file_upload(self,params):
 
-    return requests.post(self.ckan_url + '/api/3/action/resource_create',data=params,headers={"X-CKAN-API-Key": self.ckan_auth},files=[('upload', file(params["upload"]))])
+    return requests.post(self.ckan_url + '/api/3/action/resource_create',data=params,headers={"X-CKAN-API-Key": self.ckan_auth},requests_kwargs={'verify':False},files=[('upload', file(params["upload"]))])
 
   def create_tag(self,params):
 
-    return self.api.call_action('tag_create',params)
+    return self.api.call_action('tag_create',params,requests_kwargs={'verify':False})
 
   def create_tag_vocabulary(self,params):
 
-    return self.api.call_action('vocabulary_create',params)
+    return self.api.call_action('vocabulary_create',params,requests_kwargs={'verify':False})
 
   def update_tag_vocabulary(self,params):
 
-    return self.api.call_action('vocabulary_update',params)
+    return self.api.call_action('vocabulary_update',params,requests_kwargs={'verify':False})
 
   def delete_tag_vocabulary(self,params):
 
-    return self.api.call_action('vocabulary_delete',params)
+    return self.api.call_action('vocabulary_delete',params,requests_kwargs={'verify':False})
 
   def show_tag_vocabulary(self,params):
 
-    return self.api.call_action('vocabulary_show',params)
+    return self.api.call_action('vocabulary_show',params,requests_kwargs={'verify':False})
 
   def get_all_tags_from_tag_vocabulary(self,params):
 
-    return self.api.call_action('tag_list',params)
+    return self.api.call_action('tag_list',params,requests_kwargs={'verify':False})
 
   def exists_taxonomy_tag_dictionaries(self,params):
 
     try:
 
-      return self.api.call_action('vocabulary_show',id=params['id'])
+      return self.api.call_action('vocabulary_show',id=params['id'],requests_kwargs={'verify':False})
 
     except ckanapi.NotFound:
 
@@ -312,29 +318,29 @@ class RealCkanApi (ICkanApi):
 
   def add_term_translation(self,params):
 
-    return self.api.call_action('term_translation_update',params)
+    return self.api.call_action('term_translation_update',params,requests_kwargs={'verify':False})
 
   def add_term_translation_many(self,params):
 
-    return self.api.call_action('term_translation_update_many',params)
+    return self.api.call_action('term_translation_update_many',params,requests_kwargs={'verify':False})
 
   def get_packages_in_group(self,params):
 
-    return self.api.call_action('group_package_show',params)
+    return self.api.call_action('group_package_show',params,requests_kwargs={'verify':False})
 
   def get_packages_in_organization(self,params):
 
-    result = self.api.action.organization_show(id=params['id'],include_datasets=True)
+    result = self.api.call_action('organization_show',{'id':params['id'],'include_datasets':True},requests_kwargs={'verify':False})
 
     return result['packages']
 
   def delete_packages_list(self,params):
 
-    return self.api.call_action('bulk_update_delete',params)
+    return self.api.call_action('bulk_update_delete',params,requests_kwargs={'verify':False})
 
   def create_default_issue(self,params):
 
-    return self.api.call_action('issue_create',params)
+    return self.api.call_action('issue_create',params,requests_kwargs={'verify':False})
 
 # Real implementation
 class LocalCkanApi (ICkanApi):
