fa792ca71f2cdae06bd309072c1591336ad7de25
Open-Steps.org <contact@open-steps.org>
2015-06-23 12:32:49 +0200

    Fixing issue that caused an error while creating a new resource

--- ckanext/odm_theme/plugin.py
+++ ckanext/odm_theme/plugin.py
@@ -289,7 +289,6 @@ class OdmThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
   plugins.implements(plugins.IRoutes, inherit=True)
   plugins.implements(plugins.IFacets)
   plugins.implements(plugins.IPackageController, inherit=True)
-  plugins.implements(plugins.IResourceController, inherit=True)
 
   def __init__(self, *args, **kwargs):
 
@@ -481,8 +480,3 @@ class OdmThemePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
 
     odm_theme_helper.session['last_dataset'] = pkg_dict
     odm_theme_helper.session.save()
-
-  # IResourceController
-
-  def before_show(self, resource_dict):
-    log.debug('before_show: %s', resource_dict)
