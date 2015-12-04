ce87f16f748c669f8c537ad280f88d90634b2bf4
Alex Corbi <a.corbi@gmail.com>
2015-12-04 01:54:48 +0100

    fixing tests

--- ckanext/odm_theme/tests/test_validation.py
+++ ckanext/odm_theme/tests/test_validation.py
@@ -5,7 +5,7 @@ class TestValidation:
 
   def test_01_odm_taxonomy_available(self):
 
-    assert os.path.isdir("../odm-taxonomy/")
-    assert os.path.isfile("../odm-taxonomy/taxonomy_en.json")
-    assert os.path.isfile("../odm-taxonomy/taxonomy_km.json")
-    assert os.path.isfile("../odm-taxonomy/taxonomy_th.json")
+    assert os.path.isdir(os.path.abspath(os.path.join(__file__, '../../','odm-taxonomy')))
+    assert os.path.isfile(os.path.abspath(os.path.join(__file__, '../../','odm-taxonomy/taxonomy_en.json')))
+    assert os.path.isfile(os.path.abspath(os.path.join(__file__, '../../','odm-taxonomy/taxonomy_km.json')))
+    assert os.path.isfile(os.path.abspath(os.path.join(__file__, '../../','odm-taxonomy/taxonomy_th.json')))
