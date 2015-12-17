c255f57a52fba7755178a60932b8c5b21819ea8f
Alex Corbi <a.corbi@gmail.com>
2015-12-17 01:09:38 +0100

    fixing tests

--- ckanext/odm_dataset/tests/test_validation.py
+++ ckanext/odm_dataset/tests/test_validation.py
@@ -8,7 +8,7 @@ class TestValidation:
   ];
 
   json_files = [
-    os.path.abspath(os.path.join(__file__, '../../','odm_presets.json')),
+    os.path.abspath(os.path.join(__file__, '../../','odm_dataset_presets.json')),
     os.path.abspath(os.path.join(__file__, '../../','odm_dataset_schema.json'))
   ];
 
