fd35e80f97342195cb5b8e90c7d8c74a8c71adb4
Alex Corbi <a.corbi@gmail.com>
2016-01-14 09:27:21 +0100

    Fixes #251

--- ckanext/odm_dataset/fanstatic/daterangepicker-module.js
+++ ckanext/odm_dataset/fanstatic/daterangepicker-module.js
@@ -1,12 +1,15 @@
 this.ckan.module('daterangepicker-module', function($, _) {
 	return {
     options: {
+      format: 'dd/mm/yyyy',
       id: ''
     },
 		initialize: function() {
       $('[id^='+this.options.field_id+']').each(function(){
       	console.log('daterangepicker-module init for '+ $(this));
-        $(this).daterangepicker();
+        $(this).daterangepicker({
+          format: 'DD/MM/YYYY'
+        });
       });
     }
   };
