ff04cd731c72f9464c1fd96268988aa45e18f4a7
Alex Corbi <a.corbi@gmail.com>
2016-04-21 18:46:31 +0200

    fixing key decryption

--- _ci/install.sh
+++ _ci/install.sh
@@ -8,8 +8,10 @@ echo "Downloading and unzipping odm_automation"
 wget https://github.com/OpenDevelopmentMekong/odm-automation/archive/master.zip -O /tmp/odm_automation.zip
 unzip /tmp/odm_automation.zip -d /tmp/
 
-echo "decrypting private key and adding it key to ssh agent"
+echo "decrypting private key"
 openssl aes-256-cbc -K $encrypted_c70662427194_key -iv $encrypted_c70662427194_iv -in odm_tech_rsa.enc -out ~/.ssh/id_rsa -d
+
+echo "adding to ssh agent"
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa
