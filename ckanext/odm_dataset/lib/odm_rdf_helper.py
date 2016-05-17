91625f0c0d8369b45410d0095473b9ab08ce13ab
Alex Corbi <a.corbi@gmail.com>
2016-05-17 21:41:40 +0200

    better impl

--- ckanext/odm_dataset/lib/odm_rdf_helper.py
+++ ckanext/odm_dataset/lib/odm_rdf_helper.py
@@ -6,21 +6,26 @@ import logging
 import rdflib
 from rdflib import URIRef, BNode, Literal
 from rdflib.namespace import Namespace, RDF
+import traceback
 
 log = logging.getLogger(__name__)
 
-def split_multilingual_object_into_triples(subject,predicate,object_value):
+def split_multilingual_object_into_triples(triple):
   ''' Takes a triple and splits it into different localized if literal_object_value
       is multilingual (JSON object, ckanext-fluent format)
   '''
-  triples_to_return = []
+  subject = triple[0]
+  predicate = triple[1]
+  object_value = triple[2]
 
-  try:
-    json_value = json.loads(object_value);
-    for key, value in json_value.iteritems():
+  if object_value is None:
+    return []
+  elif isinstance(object_value, basestring):
+    return[(subject, predicate, Literal(object_value))]
+  else:
+    triples_to_return = []
+    for key, value in object_value.iteritems():
       if value:
         triples_to_return.append((subject, predicate, Literal(value, lang=key)))
-  except:
-    triples_to_return.append((subject, predicate, Literal(object_value)))
 
-  return triples_to_return
+    return triples_to_return
