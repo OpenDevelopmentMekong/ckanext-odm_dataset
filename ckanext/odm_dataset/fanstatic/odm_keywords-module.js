adf478bb90de1ebb439f330c5f5ae26f3795830d
Alex Corbi <a.corbi@gmail.com>
2017-03-16 22:20:10 +0100

    test

--- ckanext/odm_dataset/fanstatic/odm_keywords-module.js
+++ ckanext/odm_dataset/fanstatic/odm_keywords-module.js
@@ -6,6 +6,12 @@ this.ckan.module('odm_keywords-module', function($, _) {
 		initialize: function() {
 
 			console.log('odm_keywords-module init');
+			console.log(this.el);
+
+			this.el.select2({
+				tags: true,
+				tokenSeparators: [',', ' ']
+			});
 
 			$('[id^='+this.options.field_id+']').each(function(){
 				console.log('Converting field ' + $(this));
