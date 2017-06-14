3eb4963efd21cf6fb0c91bf1a33aabaac663031a
Alex Corbi <a.corbi@gmail.com>
2017-06-14 16:56:06 +0200

    Fixes #420

--- ckanext/odm_dataset/fanstatic/odm_keywords-module.js
+++ ckanext/odm_dataset/fanstatic/odm_keywords-module.js
@@ -23,26 +23,26 @@ function initMultiSelect(tSel) {
 		        q: term
 		      };
 		    },
-	    results: function(data, page) {
-				var suggestions;
-		    if (data.facet_counts){
-		      suggestions = data.facet_counts.facet_fields.extras_odm_keywords;
-		    }
+		    results: function(data, page) {
+					var suggestions;
+			    if (data.facet_counts){
+			      suggestions = data.facet_counts.facet_fields.extras_odm_keywords;
+			    }
 
-		    var string_suggestions = [];
-		    for (var index in suggestions){
-		      if (typeof suggestions[index] === 'string'){
-		        string_suggestions.push({
-		          "id": suggestions[index],
-		          "text": suggestions[index]
-		        });
-		      }
-		    }
+			    var string_suggestions = [];
+			    for (var index in suggestions){
+			      if (typeof suggestions[index] === 'string'){
+			        string_suggestions.push({
+			          "id": suggestions[index],
+			          "text": suggestions[index]
+			        });
+			      }
+			    }
 
-	      return {
-	        results: string_suggestions
-	      };
-	    }
+		      return {
+		        results: string_suggestions
+		      };
+		    }
 	  },
     initSelection: function (element, callback) {
       var data = [];
@@ -83,7 +83,6 @@ function initMultiSelect(tSel) {
     });
   })(tSel);
 }
-https://data.opendevelopmentmekong.net/api/3/action/tag_list?vocabulary_id=taxonomy
 this.ckan.module('odm_keywords-module', function($, _) {
 	return {
     options: {
