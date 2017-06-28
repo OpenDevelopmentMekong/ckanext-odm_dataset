455d6aca93c52dfd6a766f169dfa8161c11c625c
Alex Corbi <a.corbi@gmail.com>
2017-06-28 16:16:46 +0200

    Fixes #420

--- ckanext/odm_dataset/fanstatic/odm_keywords-module.js
+++ ckanext/odm_dataset/fanstatic/odm_keywords-module.js
@@ -74,9 +74,12 @@ function initMultiSelect(tSel) {
 
 				var newValue = $('#s2id_' + t.attr('id') + ' input ').val();
 				var enteredTaxonomies = $('#field-taxonomy').val();
+				var enteredTaxonomiesLowerCase = numbers.map(function(x) {
+				   return x.toLowerCase();
+				});
 
-				if (enteredTaxonomies.indexOf(newValue) > -1){
-					alerts("keyword " +  newValue + " has been already entered on the topic field.");
+				if (enteredTaxonomiesLowerCase.indexOf(newValue) > -1){
+					alert("keyword " +  newValue + " has been already entered on the topic field.");
 				}else{
 					t.val(t.val() + ',' + newValue);
 
